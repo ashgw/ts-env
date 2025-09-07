6486c4b97728d046a71d874d99fff444fa1348e7
ashgw <ashrefgw@proton.me>
2025-09-07 11:14:20 +0100

    add env tuple

---
+++ src/helpers.ts
@@ -0,0 +1,26 @@
+import type { Keys, UnionToTuple } from 'ts-roids';
+import { z } from 'zod';
+
+/**
+ * Create a **tuple** of env var names from a Zod schema record,
+ * keeping the key order you wrote too. This is perfect for piping into
+ * `disablePrefix` when using `createEnv`.
+ *
+ * @example
+ * const serverVars = {
+ *   NODE_ENV: z.enum(["production", "development", "test"]).optional(),
+ *   DATABASE_URL: z.string().url(),
+ *   DIRECT_URL: z.string().url(),
+ * } ;
+ *
+ * const serverVarTuple = envTuple(serverVars);
+ * //    ^? ["NODE_ENV", "DATABASE_URL", "DIRECT_URL"]
+ *
+ * @param keys Record of keys mapped to Zod schemas
+ * @returns Tuple of the keys in declaration order
+ */
+export function envTuple<
+  Schema extends Record<Uppercase<string>, z.ZodTypeAny>,
+>(keys: Schema) {
+  return Object.keys(keys) as UnionToTuple<Keys<typeof keys>>;
+}
